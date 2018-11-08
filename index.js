7a73c24da28cffef0167be8a681ffb86b74f99b3
Stefan Koolen <nast3zz@gmail.com>
2018-11-08 12:02:53 +0100

    Add /donators command

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const Telegraf = require('telegraf')
 const fetch = require("node-fetch")
-const command_list = 'live - Show a list of online streamers'
+const command_list = `
+                        donators - Show the top 10 total donators
+                        live - Show a list of online streamers
+                        `
 
 const bot = new Telegraf(process.env.TELEGRAM_KEY, {username: process.env.TELEGRAM_USERNAME})
 
@@ -14,11 +17,39 @@ const buildStreamers = (body) => {
     return streamersInfo
 }
 
+const buildDonators = (body) => {
+    let donatorsInfo = ``
+    donatorsInfo += `Top 10 total donators\n\n`
+    for (let index = 0; index < 10; index++) {
+        const element = body.donators[index];
+        if(index == 0)
+            donatorsInfo += `🥇`
+        else if(index == 1)
+            donatorsInfo += `🥈`
+        else if(index == 2)
+            donatorsInfo += `🥉`
+        donatorsInfo += element[0] + ` - ` + element[1] + ` NIM`
+        donatorsInfo += `\n`
+    }
+    return donatorsInfo
+}
+
 bot.command('live', async (ctx) => {
     try {
         const res = await fetch("https://app.twinibot.com/api/channels")
         const body = await res.json()
         ctx.reply(buildStreamers(body), {"disable_web_page_preview": true})
+    } catch (error) {
+        console.log(error)
+        ctx.reply(`Error while loading the information. Please try again later.`)
+    }
+})
+
+bot.command('donators', async (ctx) => {
+    try {
+        const res = await fetch("https://app.twinibot.com/api/donators")
+        const body = await res.json()
+        ctx.reply(buildDonators(body), {"disable_web_page_preview": true})
     } catch (error) {
         console.log(error)
         ctx.reply(`Error while loading the information. Please try again later.`)
